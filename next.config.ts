e1e157ddbae4466ab42aa9b3c507385c0baccabc
madisonmamott <madison.sss.ma@gmail.com>
2025-04-27 22:31:04 +1200

    skip eslint build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // ✅ 在 build 阶段忽略 TypeScript 报错
   },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ 在build时跳过eslint检查
+  },
 };
 
 export default nextConfig;
